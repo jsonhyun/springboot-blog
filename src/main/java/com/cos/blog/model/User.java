7130ef10b0d3eb3d1ea03e63ce3e8728691d9038
jsonhyun <airplant@naver.com>
2020-11-16 18:05:03 +0900

    model 작성

---
+++ src/main/java/com/cos/blog/model/User.java
@@ -0,0 +1,38 @@
+package com.cos.blog.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+// ORM ->Java(다른언어) Object -> 테이블로 매핑해주는 기술
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity // User 클래스가 MySQL에 테이블이 생성이 된다.
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //프로젝트에서 연결된 DB의 넘버링 전략을 따라간다.
+    private int id; //시퀀스, auto_increment
+
+    @Column(nullable = false, length = 30)
+    private String username; //아이디
+
+    @Column(nullable = false, length = 100)
+    private String password;
+
+    @Column(nullable = false, length = 50)
+    private String email;
+
+    @ColumnDefault("'user'")
+    private String role; // Enum을 쓰는게 좋다. // admin, user, manager 등
+
+    @CreationTimestamp //시간이 자동 입력
+    private Timestamp createDate;
+}
